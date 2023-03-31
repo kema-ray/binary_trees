0cf5fae7595c3c9efbdc8574980d1e0d8aa6c557
kema-ray <rachelkemuma99@gmail.com>
2023-03-31 19:21:45 +0300

    function that goes through a binary tree using level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -6,6 +6,19 @@
 #include <string.h>
 
 /**
+ * struct link_s - structure for advanced tasks
+ * @n: depth of node specified
+ * @node: node of tree to store
+ * @next: next node of the linked list
+ */
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
+
+/**
  * struct binary_tree_s - Binary tree node
  *
  * @n: Integer stored in the node
@@ -48,5 +61,11 @@ int tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+void linked_node(link_t **head, const binary_tree_t *tree, size_t level);
+void recursion(link_t **head, const binary_tree_t *tree);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
